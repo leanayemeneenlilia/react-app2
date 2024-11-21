80f36d8f43999891927fc8d607b02c8af09b1be0
leana <leana.yemene@enlilia.com>
2024-11-21 14:13:24 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('/api/hello/')
+            .then(response => response.json())
+            .then(data => setMessage(data.message));
+    }, []);
+
+    return (
+        <div>
+            <h1>React App</h1>
+            <p>Message  {message}</p>
+        </div>
+    );
 }
 
 export default App;
